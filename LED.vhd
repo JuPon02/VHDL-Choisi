67c826a5f2cf6df9208e4972805f4c5992ad962c
JuPon02 <la205630@student.helha.be>
2024-10-09 18:09:35 +0200

    Les différents fichiers

---
+++ LED.vhd
@@ -0,0 +1,13 @@
+ibrary IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity LED is
+	port(
+		 );
+end entity;
+
+architecture rtl of LED is
+	begin
+		
+end rtl;
---
+++ Switch.vhd
@@ -0,0 +1,13 @@
+ibrary IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity Switch is
+	port(
+		 );
+end entity;
+
+architecture rtl of Switch is
+	begin
+		
+end rtl;
