b94b9f0bfdc25f0a7406ee7d8a867a9eef781dcc
JuPon02 <la205630@student.helha.be>
2024-10-10 16:12:53 +0200

    Modification top level

--- Switch.vhd
+++ Switch.vhd
@@ -4,11 +4,12 @@ use IEEE.numeric_std.all;
 
 entity Switch is
 	port(
-	
-		 );
+		I_switchs : in std_logic_vector(10-1 downto 0);
+		O_switchs : out std_logic_vector(10-1 downto 0)
+		);
 end entity;
 
 architecture rtl of Switch is
 	begin
-		
+		O_switchs <= I_switchs;
 end rtl;
--- top_Choisi.vhd
+++ top_Choisi.vhd
@@ -2,12 +2,36 @@ ibrary IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
-entity LED is
+entity top_Choisi is
 	port(
 		 );
 end entity;
 
-architecture rtl of LED is
+architecture rtl of top_Choisi is
+
+	component LED
+		port(
+			i_led : in STD_LOGIC_VECTOR (10-1 downto 0);  --entrée (10 bits)
+			o_led : out STD_LOGIC_VECTOR (10-1 downto 0) --sortie (10 bits pour 10 led)
+			 );
+	end component;
+	
+	component Switch
+		port(
+			I_switchs : in std_logic_vector(10-1 downto 0);
+			O_switchs : out std_logic_vector(10-1 downto 0)
+			);
+	end component;
+	
+	component driver7s
+		port(
+			I_data : in std_logic_vector(3 downto 0);
+			O_7s : out std_logic_vector(6 downto 0);
+			CLK, RESET : in std_logic 
+			);
+	end component
+	
+	
 	begin
 		
 end rtl;
