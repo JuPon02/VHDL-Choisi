1e8127c37fec6ed639e1269bd8c40000458f51a4
naunau09 <lemanarnaud.5@gmail.com>
2024-10-10 16:29:51 +0200

    counter

--- Random.vhd
+++ Random.vhd
@@ -5,25 +5,29 @@ use IEEE.numeric_std.all;
 entity Random is
 	port(
 		CLOCK_50	: in	std_logic;
+		KEY			: in std_logic_vector(4 downto -1); --KEY(0) bouton de lancement sinon modif process ligne 18
+		val_rand	: out std_logic_vector(4 downto -1) --Valeur du compteur lors de l'appui
 		 );
 end entity;
 
 architecture rtl of Random is
 
-	signal timerCounter: std_logic_vector(3 downto 0);	
+	signal timerCounter: std_logic_vector(4 downto -1);	
 
 	begin
 	
-	process (CLOCK_50)
+	process (CLOCK_50,KEY(0))
 	begin
 		if (rising_edge(CLOCK_50)) then
 			if(TimerCounter = "1010") then
-				TimerOK <= '1';
+				TimerCounter <= "0000";
 			else
 				TimerCounter <= std_logic_vector(unsigned(TimerCounter) +  1);
 			end if;
 		end if;
+		
+		if (KEY(0) = '1') then
+		val_rand <= timerCounter; --Quand on appuie, on prend la valeur du compteur (entre 1 et 10)
 	end process;
 	
 end rtl;
---test
