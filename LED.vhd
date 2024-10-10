301e8628e1068cb95927061d02d071af92a777de
naunau09 <lemanarnaud.5@gmail.com>
2024-10-10 15:57:03 +0200

    led

--- LED.vhd
+++ LED.vhd
@@ -4,6 +4,8 @@ use IEEE.numeric_std.all;
 
 entity LED is
 	port(
+		i_led : in STD_LOGIC_VECTOR (9 downto 0);  --entrée (10 bits)
+        o_led : out STD_LOGIC_VECTOR (9 downto 0) --sortie (10 bits pour 10 led)
 		 );
 end entity;
 
