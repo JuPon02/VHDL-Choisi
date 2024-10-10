d5d2ba14758f4e793688ac55d8e9eb44cbf128fc
naunau09 <lemanarnaud.5@gmail.com>
2024-10-10 16:15:07 +0200

    counter

--- Random.vhd
+++ Random.vhd
@@ -4,11 +4,26 @@ use IEEE.numeric_std.all;
 
 entity Random is
 	port(
+		CLOCK_50	: in	std_logic;
 		 );
 end entity;
 
 architecture rtl of Random is
+
+	signal timerCounter: std_logic_vector(3 downto 0);	
+
 	begin
-		
+	
+	process (CLOCK_50)
+	begin
+		if (rising_edge(CLOCK_50)) then
+			if(TimerCounter = "1010") then
+				TimerOK <= '1';
+			else
+				TimerCounter <= std_logic_vector(unsigned(TimerCounter) +  1);
+			end if;
+		end if;
+	end process;
+	
 end rtl;
 --test
