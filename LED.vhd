2407222d69670111d6cc39b33d92a63097d5a343
JuPon02 <la205630@student.helha.be>
2024-10-10 15:59:02 +0200

    test

--- LED.vhd
+++ LED.vhd
@@ -4,13 +4,13 @@ use IEEE.numeric_std.all;
 
 entity LED is
 	port(
-		i_led : in STD_LOGIC_VECTOR (9 downto 0);  --entrée (10 bits)
-        o_led : out STD_LOGIC_VECTOR (9 downto 0) --sortie (10 bits pour 10 led)
+		i_led : in STD_LOGIC_VECTOR (10-1 downto 0);  --entrée (10 bits)
+        o_led : out STD_LOGIC_VECTOR (10-1 downto 0) --sortie (10 bits pour 10 led)
 		 );
 end entity;
 
 architecture rtl of LED is
 	begin
-		
+		-- JE ne sais pas
 end rtl;
 --test
