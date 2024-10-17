f88ad608b68e380c4649bb15cfd708615aa51fc8
JuPon02 <la205630@student.helha.be>
2024-10-17 16:49:26 +0200

    GestionBP

--- GestionBP.vhd
+++ GestionBP.vhd
@@ -5,35 +5,37 @@ use IEEE.numeric_std.all;
 entity GestionBP is
     port(
         clk : in std_logic;                  
-        KEY : in std_logic_vector(3 downto 0); -- Entrée des boutons poussoirs
+        I_BP : in std_logic_vector(3 downto 0); -- Entrée des boutons poussoirs
         O_BP : out std_logic_vector(3 downto 0) -- Sortie des boutons stables
     );
 end entity GestionBP;
 
 architecture rtl of GestionBP is
-    
+
     constant DELAY_TICKS : integer := 500000;  -- 10 ms à 50 MHz (50 MHz * 10 ms = 500 000 cycles)
+
+    -- Déclaration du type de tableau d'entiers
+    type debounce_array is array (3 downto 0) of integer;
     
-    signal debounce_counter : integer_vector(3 downto 0)   := (others => 0);
+    signal debounce_counter : debounce_array := (others => 0);
     signal stable_state     : std_logic_vector(3 downto 0) := (others => '1');
     signal previous_state   : std_logic_vector(3 downto 0) := (others => '1');
     signal button_pressed   : std_logic_vector(3 downto 0) := (others => '0');
-    
+
 begin
     process (clk)
     begin
         if rising_edge(clk) then
             for i in 0 to 3 loop
-
-                if KEY(i) /= stable_state(i) then
-                    debounce_counter(i) <= 0;						-- Si bouton change d'état, réinitialiser le compteur
+                if I_BP(i) /= stable_state(i) then
+                    debounce_counter(i) <= 0; -- Si bouton change d'état, réinitialiser le compteur
                 else
-                    if debounce_counter(i) < DELAY_TICKS then 		-- Incrémenter le compteur si l'état est stable
+                    if debounce_counter(i) < DELAY_TICKS then -- Incrémenter le compteur si l'état est stable
                         debounce_counter(i) <= debounce_counter(i) + 1;
                     end if;
 
-                    if debounce_counter(i) = DELAY_TICKS then		 -- Si le compteur atteint la valeur du seuil (10ms), l'état est considéré comme stable
-                        stable_state(i) <= KEY(i);
+                    if debounce_counter(i) = DELAY_TICKS then -- Si le compteur atteint la valeur du seuil (10ms), l'état est considéré comme stable
+                        stable_state(i) <= I_BP(i);
                     end if;
                 end if;
 
@@ -52,4 +54,3 @@ begin
     O_BP <= button_pressed;
 
 end architecture;
-
